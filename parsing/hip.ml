0a22bea550cae3dc029c93775695fa19b835ad78
songyahui <songyahuiapp@hotmail.com>
2021-07-21 13:30:27 +0800

    m

---
+++ parsing/hip.ml
@@ -0,0 +1,41 @@
+
+
+exception Foo of string
+
+
+let rec input_lines file =
+  match try [input_line file] with End_of_file -> [] with
+   [] -> []
+  | [line] -> (String.trim line) :: input_lines file
+  | _ -> failwith "Weird input_line return value"
+;;
+
+
+let string_of_program p :string =
+  Pprintast.string_of_structure p ^"\n";
+  ;;
+
+
+
+
+
+let () =
+  let inputfile = (Sys.getcwd () ^ "/" ^ Sys.argv.(1)) in
+(*    let outputfile = (Sys.getcwd ()^ "/" ^ Sys.argv.(2)) in
+print_string (inputfile ^ "\n" ^ outputfile^"\n");*)
+  let ic = open_in inputfile in
+  try
+      let lines =  (input_lines ic ) in
+      let line = List.fold_right (fun x acc -> acc ^ "\n" ^ x) (List.rev lines) "" in
+      let progs = Parser.implementation Lexer.token (Lexing.from_string line) in
+      
+      print_string (string_of_program progs ) ; 
+      flush stdout;                (* 现在写入默认设备 *)
+      close_in ic                  (* 关闭输入通道 *)
+
+    with e ->                      (* 一些不可预见的异常发生 *)
+      close_in_noerr ic;           (* 紧急关闭 *)
+      raise e                      (* 以出错的形式退出: 文件已关闭,但通道没有写入东西 *)
+
+   ;;
+
